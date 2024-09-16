ad04dcf3b81409e5136848dcd47f659240eeabd1
DobosP <pdobos@yahoo.com>
2024-09-16 21:42:10 +0300

    add initial stuff

---
+++ utils/wisper_3.py
@@ -0,0 +1,36 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=30,
+    batch_size=16,
+    return_timestamps=True,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+
+result = pipe("/home/dobo/projects/speaker/recordings/5_min.mp3",
+              generate_kwargs={"language": "english"})
+print(result["text"])
+
+with open("test.txt", "w") as writer:
+    writer.write(result["text"])
